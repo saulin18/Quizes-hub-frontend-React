0126e59eb571b8a535a36ae9bfe01e8678210db1
saulin18 <sondonsaul@gmail.com>
2024-10-09 03:55:31 -0400

    register fixed

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,11 +1,12 @@
 import { useAuthStore } from "../store/auth";
-import { authAxios, axi } from "./useAxios";
+import {  axi } from "./useAxios";
 
 
 export const registerRequest = async (username: string, password: string) => {
     try {
-         const response = await authAxios.post("/auth/register/", {username, password})
-    return response.data;} catch (error) {
+         const response = await axi.post("/auth/register/", {username, password})
+         useAuthStore.getState().setToken(response.data.access, response.data.refresh);
+        return response.data;} catch (error) {
         console.log(error);
     }
   
